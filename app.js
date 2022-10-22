32114195d623de68e3fe6e116ea02f98feee3ba6
iamshravan <86182557+iamshravan@users.noreply.github.com>
2022-10-22 21:50:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos"); 
+const generateTemplate = (todo) => {
+  const html = `
+   <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${todo}</span>
+                <i class="fa fa-trash-o delete"></i>
+    </li>
+    `;
+  list.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset(); 
+  }
+});
+
+
+
+
+
+// Deleting todos - we are using event delegation
+//we already had reference for ul
+list.addEventListener("click", (e) => {
+  //  if (e.target.tagName === "I")
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+
+
+
+//Filter and Searching - keyup event
+//cont search = document.querySelector('.search input');
+const search = document.querySelector(".input-field"); //reference to search input
+
+const filterTodos = (term) => {
+    Array.from(list.children)
+      .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+      .forEach((todo) => todo.classList.add("filtered"));
+    
+    
+    
+    Array.from(list.children)
+      .filter((todo) => todo.textContent.toLowerCase().includes(term))
+      .forEach((todo) => todo.classList.remove("filtered"));
+    
+};
+
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+    filterTodos(term);
+});
+
